3d02110b0594034607eb14544cb8cea7d734eb37
Evgeny <chiburashka47@gmail.com>
2020-12-28 12:17:16 +0300

    add redo, opponent move, music

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.scss";
 import Game from "./Components/game";
 import MainPage from "./Components/mainPage";
--- src/Components/game/index.js
+++ src/Components/game/index.js
@@ -6,108 +6,16 @@ import oImg from "../game/public/0.png";
 
 export const audio = new Audio(background_sound);
 
-const createMatrix = (size) => {
-  const matrix = [[]];
-
-  for (let i = 0; i < size; i++) {
-    matrix[i] = [];
-    for (let j = 0; j < size; j++) {
-      matrix[i][j] = null;
-    }
-  }
-
-  return matrix;
-};
-
-const copyMatrix = (oldMatrix, size) => {
-  const matrix = createMatrix(size);
-
-  for (let i = 0; i < oldMatrix.length; i++) {
-    for (let j = 0; j < oldMatrix.length; j++) {
-      matrix[i][j] = oldMatrix[i][j];
-    }
-  }
-
-  return matrix;
-};
-
-const checkWinner = (y, x, matrix) => {
-  const value = matrix[y][x];
-
-  // check positive diag
-  if (matrix[y + 1] ? matrix[y + 1][x + 1] === value : false) {
-    if (matrix[y + 2] ? matrix[y + 2][x + 2] === value : false) {
-      return value;
-    } else if (matrix[y - 1] ? matrix[y - 1][x - 1] === value : false) {
-      return value;
-    }
-  }
-
-  // check negativ diag
-  if (matrix[y - 1] ? matrix[y - 1][x - 1] === value : false) {
-    if (matrix[y - 2] ? matrix[y - 2][x - 2] === value : false) {
-      return value;
-    }
-  }
-
-  // check positive underDiag
-  if (matrix[y - 1] ? matrix[y - 1][x + 1] === value : false) {
-    if (matrix[y - 2] ? matrix[y - 2][x + 2] === value : false) {
-      return value;
-    } else if (matrix[y + 1] ? matrix[y + 1][x - 1] === value : false) {
-      return value;
-    }
-  }
-
-  // check negativ underDiag
-  if (matrix[y + 1] ? matrix[y + 1][x - 1] === value : false) {
-    if (matrix[y + 2] ? matrix[y + 2][x - 2] === value : false) {
-      return value;
-    }
-  }
-
-  // check positive horisontal
-  if (matrix[y][x + 1] === value) {
-    if (matrix[y][x + 2] === value || matrix[y][x - 1] === value) {
-      return value;
-    }
-  }
-
-  // check negativ horisontal
-  if (matrix[y][x - 1] === value) {
-    if (matrix[y][x - 2] === value) {
-      return value;
-    }
-  }
-
-  // check positive verical
-  if (matrix[y + 1] ? matrix[y + 1][x] === value : false) {
-    if (matrix[y + 2] ? matrix[y + 2][x] === value : false) {
-      return value;
-    } else if (matrix[y - 1] ? matrix[y - 1][x] === value : false) {
-      return value;
-    }
-  }
-
-  // check negativ verical
-  if (matrix[y - 1] ? matrix[y - 1][x] === value : false) {
-    if (matrix[y - 2] ? matrix[y - 2][x] === value : false) {
-      return value;
-    }
-  }
-
-  return null;
-};
-
 export default class Game extends React.Component {
   constructor(props) {
     super(props);
     this.data = JSON.parse(window.localStorage.getItem("data"));
     this.size = this.data.setting.boardSize + 3;
+    this.opponent = this.data.setting.opponent;
     this.state = {
       history: [
         {
-          matrix: createMatrix(this.size),
+          matrix: this.createMatrix(this.size),
         },
       ],
       stepNumber: 0,
@@ -116,10 +24,103 @@ export default class Game extends React.Component {
     };
   }
 
+  createMatrix = (size) => {
+    const matrix = [[]];
+
+    for (let i = 0; i < size; i++) {
+      matrix[i] = [];
+      for (let j = 0; j < size; j++) {
+        matrix[i][j] = null;
+      }
+    }
+
+    return matrix;
+  };
+
+  copyMatrix = (oldMatrix, size) => {
+    const matrix = this.createMatrix(size);
+
+    for (let i = 0; i < oldMatrix.length; i++) {
+      for (let j = 0; j < oldMatrix.length; j++) {
+        matrix[i][j] = oldMatrix[i][j];
+      }
+    }
+
+    return matrix;
+  };
+
+  checkWinner = (y, x, matrix) => {
+    const value = matrix[y][x];
+
+    // check positive diag
+    if (matrix[y + 1] ? matrix[y + 1][x + 1] === value : false) {
+      if (matrix[y + 2] ? matrix[y + 2][x + 2] === value : false) {
+        return value;
+      } else if (matrix[y - 1] ? matrix[y - 1][x - 1] === value : false) {
+        return value;
+      }
+    }
+
+    // check negativ diag
+    if (matrix[y - 1] ? matrix[y - 1][x - 1] === value : false) {
+      if (matrix[y - 2] ? matrix[y - 2][x - 2] === value : false) {
+        return value;
+      }
+    }
+
+    // check positive underDiag
+    if (matrix[y - 1] ? matrix[y - 1][x + 1] === value : false) {
+      if (matrix[y - 2] ? matrix[y - 2][x + 2] === value : false) {
+        return value;
+      } else if (matrix[y + 1] ? matrix[y + 1][x - 1] === value : false) {
+        return value;
+      }
+    }
+
+    // check negativ underDiag
+    if (matrix[y + 1] ? matrix[y + 1][x - 1] === value : false) {
+      if (matrix[y + 2] ? matrix[y + 2][x - 2] === value : false) {
+        return value;
+      }
+    }
+
+    // check positive horisontal
+    if (matrix[y][x + 1] === value) {
+      if (matrix[y][x + 2] === value || matrix[y][x - 1] === value) {
+        return value;
+      }
+    }
+
+    // check negativ horisontal
+    if (matrix[y][x - 1] === value) {
+      if (matrix[y][x - 2] === value) {
+        return value;
+      }
+    }
+
+    // check positive verical
+    if (matrix[y + 1] ? matrix[y + 1][x] === value : false) {
+      if (matrix[y + 2] ? matrix[y + 2][x] === value : false) {
+        return value;
+      } else if (matrix[y - 1] ? matrix[y - 1][x] === value : false) {
+        return value;
+      }
+    }
+
+    // check negativ verical
+    if (matrix[y - 1] ? matrix[y - 1][x] === value : false) {
+      if (matrix[y - 2] ? matrix[y - 2][x] === value : false) {
+        return value;
+      }
+    }
+
+    return null;
+  };
+
   handleClick(indexColumn, indexRow) {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
-    const matrix = copyMatrix(current.matrix, this.size);
+    const matrix = this.copyMatrix(current.matrix, this.size);
 
     if (this.state.winner || matrix[indexColumn][indexRow]) {
       return;
@@ -127,7 +128,21 @@ export default class Game extends React.Component {
 
     matrix[indexColumn][indexRow] = this.state.xIsNext ? "X" : "0";
 
-    const winner = checkWinner(indexColumn, indexRow, matrix);
+    let winner = this.checkWinner(indexColumn, indexRow, matrix);
+
+    if (this.opponent) {
+      let indexColumn = Math.floor(Math.random() * this.size),
+        indexRow = Math.floor(Math.random() * this.size);
+
+      while (matrix[indexColumn][indexRow]) {
+        indexColumn = Math.floor(Math.random() * this.size);
+        indexRow = Math.floor(Math.random() * this.size);
+      }
+      matrix[indexColumn][indexRow] = "0";
+      if (!winner) {
+        winner = this.checkWinner(indexColumn, indexRow, matrix);
+      }
+    }
 
     this.setState({
       history: history.concat([
@@ -136,55 +151,58 @@ export default class Game extends React.Component {
         },
       ]),
       stepNumber: history.length,
-      xIsNext: !this.state.xIsNext,
+
+      xIsNext: this.opponent ? this.state.xIsNext : !this.state.xIsNext,
       winner: winner,
     });
   }
 
   render() {
-    // audio.play();
-    // var playPromise = audio.play();
+    let playPromise = audio.play();
 
-    // //111111111111111111111111111111111111111111111111111
-    // if (playPromise !== undefined) {
-    //   playPromise
-    //     .then((_) => {
+    if (playPromise !== undefined) {
+      playPromise.catch((error) => {
+        window.addEventListener("load", () => {
+          audio.play();
+        });
+      });
+    }
+    audio.addEventListener("ended", () => audio.play());
 
-    //       console.log("audio played auto");
-    //     })
-    //     .catch((error) => {
-
-    //       console.log("playback prevented");
-    //     });
-    // }
-    // audio.addEventListener("ended", () => audio.play());
-    console.log(this.state, "last state render");
-    console.log(
-      this.state.history[this.state.stepNumber].matrix,
-      "last state render"
-    );
     const history = this.state.history;
-    let current = history[this.state.stepNumber];
+    const current = history[this.state.stepNumber];
 
     return (
       <div className="game">
         <BackBtn />
-        <div className="game__nav">
+        <div className="game__nav ">
           <div
             onClick={() => {
-              this.setState({ stepNumber: this.state.stepNumber - 1 });
-              current = history[this.state.stepNumber];
+              if (this.state.stepNumber === 0 || this.state.winner) {
+                return;
+              }
+              this.setState({
+                stepNumber: this.state.stepNumber - 1,
+                xIsNext: this.opponent
+                  ? this.state.xIsNext
+                  : !this.state.xIsNext,
+              });
             }}
-            className="game__nav__btn"
+            className={`game__nav__btn ${
+              this.state.stepNumber && !this.state.winner ? "active" : ""
+            }`}
           >
             Undo
           </div>
           <div
             onClick={() => {
+              if (this.state.stepNumber === 0) {
+                return;
+              }
               this.setState({
                 history: [
                   {
-                    matrix: createMatrix(this.size),
+                    matrix: this.createMatrix(this.size),
                   },
                 ],
                 stepNumber: 0,
@@ -192,11 +210,35 @@ export default class Game extends React.Component {
                 winner: null,
               });
             }}
-            className="game__nav__btn"
+            className={`game__nav__btn ${
+              this.state.stepNumber ? "active" : ""
+            }`}
           >
             Reset
           </div>
-          <div className="game__nav__btn">Redo</div>
+          <div
+            onClick={() => {
+              if (
+                this.state.stepNumber === this.state.history.length - 1 ||
+                this.state.winner
+              ) {
+                return;
+              }
+              this.setState({
+                stepNumber: this.state.stepNumber + 1,
+                xIsNext: this.opponent
+                  ? this.state.xIsNext
+                  : !this.state.xIsNext,
+              });
+            }}
+            className={`game__nav__btn ${
+              this.state.stepNumber < this.state.history.length - 1
+                ? "active"
+                : ""
+            }`}
+          >
+            Redo
+          </div>
         </div>
         <div className="game__status">
           {this.state.winner
@@ -205,7 +247,6 @@ export default class Game extends React.Component {
         </div>
         <div className="game__board">
           {current.matrix.map((item, indexColumn) => {
-            console.log(this.state.stepNumber);
             return (
               <div key={indexColumn} className="game__board__row">
                 {item.map((item, indexRow) => {
